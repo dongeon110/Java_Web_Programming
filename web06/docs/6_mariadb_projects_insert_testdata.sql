a6ba29485842159f9e8e8980947440aadda649d6
dongeon110 <dongeon110@gmail.com>
2022-09-13 19:02:56 +0900

    220913 Study

---
+++ web06/docs/6_mariadb_projects_insert_testdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO projects (pname, content, sta_date, end_date, state, cre_date) VALUES 
+	("프로젝트1", "내용내용1", NOW(), NOW(), 1, NOW()),
+	("프로젝트2", "내용내용2", NOW(), NOW(), 1, NOW()),
+	("프로젝트3", "내용내용3", NOW(), NOW(), 1, NOW()),
+	("프로젝트4", "내용내용4", NOW(), NOW(), 1, NOW()),
+	("프로젝트5", "내용내용5", NOW(), NOW(), 1, NOW());
+
+SELECT * FROM projects;
